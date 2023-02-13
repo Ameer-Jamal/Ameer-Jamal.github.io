74a382834cac48419061bc83d2ec28238116761f
Ameer Jamal <ameer10194@gmail.comm>
2023-02-13 15:26:52 -0700

    Fixed submit and reset, added education article, fixed many css bugs,

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,17 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+
+
+function submitAndResetForm() {
+    document.getElementById("contactForm").submit();
+	document.getElementById("contactForm").reset();
+
+}
+
+
+
+
 (function($) {
 
 	var	$window = $(window),
